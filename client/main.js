383647cc92d48a2ea3e6f03cc6623ad2a47dab57
Jordy Webb <webb.jordy@gmail.com>
2017-04-12 06:14:24 -0700

    recactor router to use dynamic urls

--- client/main.js
+++ client/main.js
@@ -9,7 +9,8 @@ import '../imports/startup/simple-schema-configuration.js';
 
 Tracker.autorun(() => {
   const isAuthenticated = !!Meteor.userId();
-  onAuthChange(isAuthenticated);
+  const currentPagePrivacy = Session.get('currentPagePrivacy');
+  onAuthChange(isAuthenticated, currentPagePrivacy);
 });
 
 Tracker.autorun(() => {
