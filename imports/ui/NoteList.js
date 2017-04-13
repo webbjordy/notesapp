e0ef4d0de32220e7e6d28248fae00d03ab1b3311
Jordy Webb <webb.jordy@gmail.com>
2017-04-13 09:27:10 -0700

    refactor NoteListHeader tests

--- imports/ui/NoteList.js
+++ imports/ui/NoteList.js
@@ -30,7 +30,11 @@ export default createContainer(() => {
   Meteor.subscribe('notes');
 
   return {
-    notes: Notes.find().fetch().map((note) => {
+    notes: Notes.find({}, {
+      sort: {
+        updatedAt: -1
+      }
+    }).fetch().map((note) => {
       return {
         ...note,
         selected: note._id === selectedNoteId
--- imports/ui/NoteListHeader.test.js
+++ imports/ui/NoteListHeader.test.js
@@ -4,17 +4,28 @@ import expect from 'expect';
 import { mount } from 'enzyme';
 
 import { NoteListHeader } from './NoteListHeader';
+import { notes } from '../fixtures/fixtures';
 
 if(Meteor.isClient) {
   describe('Note List Header', function () {
+    let meteorCall;
+    let Session;
+
+    beforeEach(function () {
+      meteorCall = expect.createSpy();
+      Session = {
+        set: expect.createSpy()
+      }
+    });
 
     it('Should call meteorCall onClick', function () {
-      const spy = expect.createSpy();
-      const wrapper = mount(<NoteListHeader meteorCall={spy} />)
+      const wrapper = mount(<NoteListHeader meteorCall={meteorCall} Session={Session} />)
 
       wrapper.find('button').simulate('click');
+      meteorCall.calls[0].arguments[1](undefined, notes[0]._id)
 
-      expect(spy).toHaveBeenCalledWith('notes.insert');
+      expect(meteorCall.calls[0].arguments[0]).toBe('notes.insert');
+      expect(Session.set).toHaveBeenCalledWith('selectedNoteId', notes[0]._id )
     });
 
   });
